a5522a127c6bf219b181c0af83b2e2ae782b5dc1
DainShin <di.shin0611@gmail.com>
2024-05-14 14:03:50 -0400

    converted project to use ViewBinding

--- ICE2/app/src/main/java/ca/georgiancollege/ice2/MainActivity.kt
+++ ICE2/app/src/main/java/ca/georgiancollege/ice2/MainActivity.kt
@@ -6,16 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    //
+    private lateinit var binding: ActivityMainBinding
 
-    //@SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+
+        // create a reference to the ActivityMainBinding Class object
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +31,7 @@ class MainActivity : AppCompatActivity()
         }
 
 
-        val helloWorldTextView = findViewById<TextView>(R.id.helloWordTextView)
+        val helloWorldTextView = binding.helloWorldTextView
         helloWorldTextView.text = getString(R.string.hello_to_dain)
     }
 }
