cbbe379f928a17c6a1b067f2b0a627870547b53b
DainShin <di.shin0611@gmail.com>
2024-06-04 14:16:42 -0400

    added simple operand entry

--- ICE5/app/src/main/java/ca/georgiancollege/ice5/Calculator.kt
+++ ICE5/app/src/main/java/ca/georgiancollege/ice5/Calculator.kt
@@ -1,4 +1,66 @@
 package ca.georgiancollege.ice5
 
-class Calculator {
+import ca.georgiancollege.ice5.databinding.ActivityMainBinding
+
+// constructor
+class Calculator(dataBinding: ActivityMainBinding)
+{
+    private var binding: ActivityMainBinding = dataBinding
+    private var result: String
+
+    init {
+        result = ""
+        createButtonsReferences()
+    }
+
+    //
+    private fun createButtonsReferences() {
+        val operandButtons = arrayOf(
+            binding.oneButton, binding.twoButton, binding.threeButton, binding.fourButton,
+            binding.fiveButton, binding.sixButton, binding.sevenButton, binding.eightButton,
+            binding.nineButton, binding.zeroButton, binding.plusMinusButton, binding.decimalButton,
+            binding.deleteButton
+        )
+
+        val operatorButtons = arrayOf(
+            binding.minusButton, binding.plusButton, binding.multiplyButton, binding.divideButton,
+            binding.percentButton, binding.clearButton
+        )
+
+        operandButtons.forEach { it.setOnClickListener { operandHandler(it.tag as String) } }
+
+        operatorButtons.forEach { it.setOnClickListener { operatorHandler(it.tag as String) } }
+    }
+
+    //
+    private fun operandHandler(tag: String) = when(tag)
+    {
+        "." -> {
+
+        }
+        "delete" -> {
+
+        }
+        "plus_minus" ->  {
+
+        }
+        else -> {
+
+            if(binding.resultTextView.text == "0")
+            {
+                binding.resultTextView.text = tag
+            }
+            else
+            {
+                result += tag
+                binding.resultTextView.text = result
+            }
+        }
+    }
+
+    //
+    private fun operatorHandler(tag: String)
+    {
+        binding.resultTextView.text = tag
+    }
 }
