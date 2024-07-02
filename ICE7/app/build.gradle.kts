0dff831dc991964c5d9df8e62334af124638dd25
DainShin <di.shin0611@gmail.com>
2024-07-02 14:05:40 -0400

    added dependencies for room

--- ICE7/app/build.gradle.kts
+++ ICE7/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -41,12 +42,26 @@ android {
 }
 
 dependencies {
-
+    // ui frame works
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // room implementations
+//    implementation(libs.androidx.core.room.common)
+//    implementation(libs.androidx.room.ktx)
+//    implementation(libs.androidx.room.runtime)
+//    kapt(libs.androidx.room.complier)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.common)
+    kapt(libs.androidx.room.complier)
+
+    // testing frameworks
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- ICE7/app/src/main/java/ca/georgiancollege/dain/ice7/MainActivity.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/dain/ice7/MainActivity.kt
@@ -14,20 +14,22 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // create an array of TV shows (mock data)
-        val favouriteTVShows = arrayOf(
-            TVShow("House of the Dragon", "HBO"),
-            TVShow("Lord of the Rings", "Prime Video"),
-            TVShow("Andor", "Disney"),
-            TVShow("Severance", "AppleTv"),
-            TVShow("Star Trek: Strange New Worlds", "Paramount+")
-        )
+//        // create an array of TV shows (mock data)
+//        val favouriteTVShows = arrayOf(
+//            TVShow("House of the Dragon", "HBO"),
+//            TVShow("Lord of the Rings", "Prime Video"),
+//            TVShow("Andor", "Disney"),
+//            TVShow("Severance", "AppleTv"),
+//            TVShow("Star Trek: Strange New Worlds", "Paramount+")
+//        )
+//
+//        // create an instance of the FirstAdapter and pass in the array of TV shows
+//        val firstAdapter = FirstAdapter(favouriteTVShows)
+//        binding.firstRecyclerView.apply {
+//            layoutManager = LinearLayoutManager(context)
+//            adapter = firstAdapter
+//        }
 
-        // create an instance of the FirstAdapter and pass in the array of TV shows
-        val firstAdapter = FirstAdapter(favouriteTVShows)
-        binding.firstRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = firstAdapter
-        }
+
     }
 }
