4c5602ef53b045f49fd0d41a38dff64d5a9f581f
DainShin <di.shin0611@gmail.com>
2024-08-01 20:36:48 -0400

    added BaseController and updated OceanController & IslandController

--- ICE11/Assets/[Scripts]/IslandController.cs
+++ ICE11/Assets/[Scripts]/IslandController.cs
@@ -1,39 +1,19 @@
 using UnityEngine;
 
-public class IslandController : MonoBehaviour
+public class IslandController : BaseController
 {
-    public float maxVertical;
-    public float minVertical;
     public float maxHorizontal;
     public float minHorizontal;
-    public float verticalSpeed;
-    void Start()
-    {
-        ResetGameObject();
-    }
 
-    void Update()
-    {
-        Move();
-        CheckBounds();
-    }
-
-    void ResetGameObject()
+   
+    public override void ResetGameObject()
     {
         var randomXPosition = Random.Range(minHorizontal, maxHorizontal);
-        transform.position = new Vector3(randomXPosition, maxVertical, 0.0f);
+        transform.position = new Vector3(randomXPosition, boundary.max, 0.0f);
     }
 
-    void Move()
+    public override void Move()
     {
         transform.position += new Vector3(0.0f, -verticalSpeed * Time.deltaTime, 0.0f);
-    }
-
-    void CheckBounds()
-    {
-        if (transform.position.y <= minVertical)
-        {
-            ResetGameObject();
-        }
     }
 }
