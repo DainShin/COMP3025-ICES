806e4739ff25fb02e7e9b72aa2d176b4ed70421d
DainShin <di.shin0611@gmail.com>
2024-08-01 23:06:23 -0400

    added BaseController in the CloudController

--- ICE11/Assets/[Scripts]/CloudController.cs
+++ ICE11/Assets/[Scripts]/CloudController.cs
@@ -1,12 +1,10 @@
 using UnityEngine;
 
-public class CloudController : MonoBehaviour
+public class CloudController : BaseController
 {
     [Header("Movement Properties")]
-    public float minVertical;
     public float minHorizontal;
     public float maxHorizontal;
-
     public float minOffscreenVertical;
     public float maxOffscreenVertical;
 
@@ -19,22 +17,10 @@ public class CloudController : MonoBehaviour
     [Range(-2.0f, 2.0f)]
     public float maxHorizontalSpeed;
 
-    public float verticalSpeed;
     public float horizontalSpeed;
 
-    void Start()
-    {
-        ResetGameObject();
-    }
 
-
-    void Update()
-    {
-        Move();
-        CheckBounds();
-    }
-
-    void ResetGameObject()
+    public override void ResetGameObject()
     {
         var randomXPostion = Random.Range(minHorizontal, maxHorizontal);
         var randomYPostion = Random.Range(minOffscreenVertical, maxOffscreenVertical);
@@ -43,14 +29,14 @@ public class CloudController : MonoBehaviour
         transform.position = new Vector3(randomXPostion, randomYPostion, 0.0f);
     }
 
-    void Move()
+    public override void Move()
     {
         transform.position += new Vector3(-horizontalSpeed * Time.deltaTime, -verticalSpeed * Time.deltaTime, 0.0f);
     }
 
-    void CheckBounds()
+    public override void CheckBounds()
     {
-        if (transform.position.y <= minVertical)
+        if (transform.position.y <= boundary.min)
         {
             ResetGameObject();
         }
